184e104c8b369eb80dfba402b56cd3438de30278
Yuliya Kudrina <y.kudrina@y-kudrina-mac.local>
2022-08-09 14:33:33 +0300

    done

--- src/test/java/github/SelenideRepository.java
+++ src/test/java/github/SelenideRepository.java
@@ -1,10 +1,10 @@
 package github;
 
-import com.codeborne.selenide.*;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.byText;
 
 public class SelenideRepository {
 
@@ -15,19 +15,17 @@ public class SelenideRepository {
         open("https://github.com/");
         // ввести в поле поиска selenide и нажать Enter
         $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
-
         // нажимаем на линк от первого результата поиска
         $$("ul.repo-list li").first().$("a").click();
-        // check: в заголовке встречается selenide/selenide
-        $("#repository-container-header").shouldHave(text("selenide / selenide"));
-
-        // ARRANGE
-        // ACT
-        // ACT
-        // ACT
-        // ASSERT
-        // ACT
-        // ASSERT
-
+        // переходим на таб Wiki
+        $("#wiki-tab").click();
+        // проверяем, что есть текст Soft assertions
+        $("#wiki-body").shouldHave(text("Soft assertions"));
+        // переходим на Soft assertions
+        $("#wiki-body").find(byText("Soft assertions")).click();
+        // ищем пример кода для JUnit5
+        $("#wiki-content").shouldHave(text("Using JUnit5 extend test class"));
+        // задержка для просмотра экрана
+        sleep(10000);
     }
 }
